3193b0fd70235aafa8d582c81fe403cb69d514a0
zohairhashmi17 <45271874+zohairhashmi17@users.noreply.github.com>
2019-01-12 22:31:36 +0500

    Add files via upload

---
+++ working with variables and datatypes.py
@@ -0,0 +1,12 @@
+#working with variables and data types
+print("he is a good man and his name is Williams")
+print('Williams has a age of 18')
+
+name = 'Williams'
+age = "18"
+print('he is a good man and his name is' + ' ' + name)
+print(name + ' ' + 'has a age of' + ' ' + age)
+
+name = "Ahmed"
+print('There is another guy named' + ' ' + name)
+print(name + "'s age is" + ' ' + age)
