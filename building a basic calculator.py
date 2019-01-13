68fd63b05b1d299ae209e72c91aa5c0135c55f25
zohairhashmi17 <45271874+zohairhashmi17@users.noreply.github.com>
2019-01-13 22:19:14 +0500

    Add files via upload

---
+++ building a basic calculator.py
@@ -0,0 +1,16 @@
+#building a basic calculator
+num1 = input("enter a number: ")
+num2 = input("enter another number: ")
+result = num1 + num2
+print(result)
+
+num1 = eval(input("enter a number: "))
+num2 = eval(input("enter another number: "))
+result = num1 + num2
+print(result)
+
+num1 = float(input("enter a number: "))
+num2 = float(input("enter another number: "))
+result = num1 + num2
+print(result)
+
