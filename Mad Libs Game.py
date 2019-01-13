d31ddf4c127e94875561f0b11b25fc383ea30954
zohairhashmi17 <45271874+zohairhashmi17@users.noreply.github.com>
2019-01-13 22:27:47 +0500

    Add files via upload

---
+++ Mad Libs Game.py
@@ -0,0 +1,8 @@
+#Mad Libs Game
+color = input("enter a color: ")
+plural_noun = input("enter a plural noun: ")
+celebrity = input("enter a celebrity: ")
+
+print("My skin color is not " + color)
+print("Black " + plural_noun + " have great bodies")
+print("Yesterday I met " + celebrity)
